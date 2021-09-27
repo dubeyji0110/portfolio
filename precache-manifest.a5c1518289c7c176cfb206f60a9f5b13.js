44762bb2591192adb15cd4b6231905a97fe72393
Devansh Dubey <devanshdubey2001@gmail.com>
2021-09-27 19:40:49 +0530

    Updates

---
+++ precache-manifest.a5c1518289c7c176cfb206f60a9f5b13.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fc5e1d24b7742737dcde68795e28b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37dec55840f9344de7c",
+    "url": "/static/css/main.a4249188.chunk.css"
+  },
+  {
+    "revision": "f5441d12171bce3eb105",
+    "url": "/static/js/2.c77c7349.chunk.js"
+  },
+  {
+    "revision": "b37dec55840f9344de7c",
+    "url": "/static/js/main.44fbe94f.chunk.js"
+  },
+  {
+    "revision": "013accdb19d35cca820f",
+    "url": "/static/js/runtime-main.4989ec99.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
